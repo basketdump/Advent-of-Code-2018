49d4031c6ab4f1cabe5c8ef37ba84663de5bff60
Tyler <tylerisop@gmail.com>
2018-12-10 20:34:44 -0600

    Part 2

--- Day3/main.py
+++ Day3/main.py
@@ -20,6 +20,15 @@ def findOverlap(fabric):
     return count
 
 
+def isOverlap(claim, fabric):
+    for i in range(claim.height):
+        for j in range(claim.width):
+            if fabric.matrix[claim.x+j][claim.y+i] > 1:
+                return True
+    
+    return False
+
+
 fab = Fabric(1000, 1000)
 claims = []
 
@@ -30,4 +39,9 @@ with open('input.txt') as f:
 for c in claims:
     representClaim(c, fab)
 
-print(findOverlap(fab))
+print("Part 1:", findOverlap(fab))
+
+for c in claims:
+    if not isOverlap(c, fab):
+        print("Part 2:", c.id)
+        break
